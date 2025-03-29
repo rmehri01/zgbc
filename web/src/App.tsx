e719dc86ad2999724836debe65afd4a5065c982e
Ryan Mehri <ryan.mehri1@gmail.com>
2025-03-28 21:59:03 -0700

    fix: css positioning

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,15 @@ import { useSetupAudio } from "./audio";
 import { useEffect, useRef, useState } from "react";
 import { useSetupSaving } from "./saving";
 
+declare global {
+  interface Document {
+    webkitExitFullscreen?(): Promise<void>;
+  }
+  interface Element {
+    webkitRequestFullscreen?(): Promise<void>;
+  }
+}
+
 function App() {
   const [width, setWidth] = useState<number>(window.innerWidth);
 
@@ -37,37 +46,50 @@ function App() {
       />
 
       {isMobile && (
-        <div className="button-container">
-          <div className="left-button-container">
-            <div className="set" draggable="false">
-              <div className="d-pad" draggable="false">
-                <a className="up" draggable="false" />
-                <a className="right" draggable="false" />
-                <a className="down" draggable="false" />
-                <a className="left" draggable="false" />
+        <>
+          <div className="middle-container">
+            <a
+              className="tm-text"
+              href="https://github.com/rmehri01/zgbc"
+              target="_blank"
+              rel="noreferrer"
+            >
+              zgbc
+            </a>
+          </div>
+
+          <div className="button-container">
+            <div className="left-button-container">
+              <div className="set" draggable="false">
+                <div className="d-pad" draggable="false">
+                  <a className="up" draggable="false" />
+                  <a className="right" draggable="false" />
+                  <a className="down" draggable="false" />
+                  <a className="left" draggable="false" />
+                </div>
               </div>
-            </div>
 
-            <a className="select">
-              <p className="slanted-text">SELECT</p>
-            </a>
-          </div>
-
-          <div className="right-button-container">
-            <div className="ab-container">
-              <a className="a">
-                <p className="slanted-text">A</p>
-              </a>
-              <a className="b">
-                <p className="slanted-text">B</p>
+              <a className="select">
+                <p className="slanted-text">SELECT</p>
               </a>
             </div>
 
-            <a className="start">
-              <p className="slanted-text">START</p>
-            </a>
+            <div className="right-button-container">
+              <div className="ab-container">
+                <a className="a">
+                  <p className="slanted-text">A</p>
+                </a>
+                <a className="b">
+                  <p className="slanted-text">B</p>
+                </a>
+              </div>
+
+              <a className="start">
+                <p className="slanted-text">START</p>
+              </a>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
